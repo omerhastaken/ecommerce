04da13de99a73a5d52db5e89490b61dddd2b8128
omerhastaken <omerefesermet@gmail.com>
2024-01-30 21:12:49 +0300

    modified:   src/App.js 	new file:   src/Context/ShopContext.jsx 	modified:   src/Pages/ShopCategory.jsx 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import { Product } from './Pages/Product';
 import { Cart } from "./Pages/Cart"
 import { LoginSignup } from './Pages/LoginSignup';
 import { Footer } from './Components/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -15,9 +18,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop/>}/>
-          <Route path='/mens' element={<ShopCategory category="mens"/>}/>
-          <Route path='/womens' element={<ShopCategory category="womens"/>}/>
-          <Route path='/kids' element={<ShopCategory category="kids"/>}/>
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="mens"/>}/>
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category="womens"/>}/>
+          <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kids"/>}/>
           <Route path="/product" element={<Product/>} />
           <Route path=":productId" element={<Product/>} />
           <Route path='/cart' element={Cart}/>
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
 import { Hero } from '../Components/Hero/Hero'
+import { ShopContext } from '../Context/ShopContext'
+import dropdown from '../Components/Assets/dropdown_icon.png'
+import Item from '../Components/Item'
 
-export const ShopCategory = () => {
+export const ShopCategory = (props) => {
+  const {all_product} = useContext(ShopContext)
   return (
     <Container>
-        <Hero />
+        <img src={props.banner} />
+        <indexSort>
+          <p>
+            <span>Showing 1-12</span> out of 36 products  
+          </p>
+          <Sort>
+            Sort by <img src={dropdown} alt="" />
+          </Sort>
+        </indexSort>
+        <Products>
+          {all_product.map((item,i)=>{
+            if (props.category===item.category) {
+              return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            }
+            else{
+              return null;
+            }
+          })}
+        </Products>
     </Container>
   )
 }
@@ -14,4 +36,9 @@ const Container = styled.div`
 
 `
 
+const indexSort = styled.div``
+
+const Sort = styled.div``
+
+const Products = styled.div``
 
